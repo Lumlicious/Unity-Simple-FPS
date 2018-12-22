c2f8a4d44d68bf854ab3932ea1342fee3d9bcbca
Chad Lumley <crlumley@gmail.com>
2018-12-21 20:48:18 -0600

    CHAD - Adding project for backup

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+	// Serialize field to store the prefab
+	[SerializeField] private GameObject enemyPrefab;
+	// Store a reference to the instance of the enemyPrefab prefab
+	private GameObject _enemy;
+
+	// Update is called once per frame
+	void Update () {
+		
+		// If instance has been destroyed instiantiate a new one
+		// ** note: enemy is not actually destroyed, just dereferenced in the scene graph
+		if(_enemy == null) {
+			float angle = Random.Range(0, 360);
+
+			_enemy = Instantiate(enemyPrefab) as GameObject;
+			_enemy.transform.position = new Vector3(0, 1, 0);
+			_enemy.transform.Rotate(0, angle, 0);
+		}
+
+	}
+}
